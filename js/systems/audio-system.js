f3ec04137c18c473515db769004e982a0fc2a027
muflihtsani9 <muflihtsani9@gmail.com>
2026-06-13 19:21:56 +0700

    update

--- js/systems/audio-system.js
+++ js/systems/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Hold Me Tight',
-    artist:    'Skyline',
+    title:     'All of Me',
+    artist:    'John Legend',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 50,       // seconds — where playback begins
